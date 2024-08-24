f2c2e8a74d2043e2e1a17b7f3fc474f8c2d7325e
Shrey Jain <130434847+shrey3219@users.noreply.github.com>
2024-08-24 22:29:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions ={
-    origin:'https://shreydev-netflixclone.onrender.com',
+    origin:'https://shreydev-netflixclone-11.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
